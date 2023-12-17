2fb2baf60e24192a7023b1e937ffb763f7cdb594
camigrandy <camgany@gmail.com>
2023-12-17 19:13:37 -0400

    marketing page added

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../components/Layout";
+import Marketing from "../pages/Marketing";
 
 const router = createBrowserRouter([
     {
@@ -11,7 +12,7 @@ const router = createBrowserRouter([
         { path: "/cio", element: <h1>CIO</h1> },
         { path: "/gerencia", element: <h1>Gerencia</h1> },
         { path: "/software", element: <h1>Software</h1> },
-        { path: "/marketing", element: <h1>Marketing</h1> },
+        { path: "/marketing", element: <Marketing /> },
         { path: "/seguridad", element: <h1>Seguridad</h1> },
         { path: "/it", element: <h1>IT</h1> },
         { path: "/contacto", element: <h1>Contacto</h1> },
